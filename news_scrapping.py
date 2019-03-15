8eedc3752646b4d7cd26942f2af74625dfae05da
sri <sripranathi.vr109@gmail.com>
2019-03-15 13:28:46 -0400

    Scrapping google news website and returning top 5 news headlines

---
+++ news_scrapping.py
@@ -0,0 +1,35 @@
+import bs4
+import html
+import json
+import re
+import requests
+import sys
+import unicodedata
+import pprint
+
+
+def soupify_event_page(url='https://news.google.com/?hl=en-US&gl=US&ceid=US:en'):
+    try:
+        r = requests.get(url)
+    except:
+        return
+    content = r.content
+    soup = bs4.BeautifulSoup(content, 'html.parser')
+
+    return soup
+
+def main():
+    soup = soupify_event_page()
+    if not soup:
+        sys.exit(1)
+
+    events_divs = soup.findAll('a',{'class':'DY5T1d'})
+    news = set()
+    for i in range(5):
+        #print(events_divs[i].text.strip())
+        news.add(events_divs[i].text.strip())
+    return news
+
+if __name__ == '__main__':
+    events = main()
+    #print(events)
