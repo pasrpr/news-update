69b810b840c923e723414cb626124b612e05096d
sri <sripranathi.vr109@gmail.com>
2019-03-16 15:27:52 -0400

    Combining all news

---
+++ news.py
@@ -0,0 +1,10 @@
+from google_news import news as google_news
+from bbc_news import news as bbc_news
+from cnn_news import news as cnn_news
+
+def main():
+    news = google_news()+bbc_news()+cnn_news()
+    return news
+
+if __name__ == '__main__':
+    news = main()
