13ae11f6ad74a559427a8e8e9feb32e5cbc3f1b7
sri <sripranathi.vr109@gmail.com>
2019-04-21 19:25:49 -0400

    Updating better ui

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from app import app
 from flask import render_template, request
 # from .news import main as updates
-from google_news import news as google_news
-from bbc_news import news as bbc_news
-from cnn_news import news as cnn_news
+from .google_news import news as google_news
+from .bbc_news import news as bbc_news
+from .cnn_news import news as cnn_news
 
 @app.route('/')
 @app.route('/index')
@@ -19,4 +19,4 @@ def result():
     news = google_news().copy()
     news.update(bbc_news())
     news.update(cnn_news())
-    return render_template('result.html', title='News', news = news)
+    return render_template('result.html', title='News', news = news.keys(), urls = news.values())
